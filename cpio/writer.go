82bc57bd3391740eb1c6d00c9cb4ef9a2f5a7d10
qeed <qeed.quan@gmail.com>
2017-02-14 21:15:43 -0500

    add cpio package

---
+++ cpio/writer.go
@@ -0,0 +1,79 @@
+package cpio
+
+import (
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+)
+
+var (
+	ErrTooLarge = errors.New("cpio: file too large")
+)
+
+type Writer struct {
+	w  io.Writer
+	b  *bufio.Writer
+	wn int
+}
+
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{
+		w: w,
+		b: bufio.NewWriter(w),
+	}
+}
+
+func (cw *Writer) Close() error {
+	cw.flush()
+	return cw.b.Flush()
+}
+
+func (cw *Writer) flush() {
+	if cw.wn <= 0 {
+		return
+	}
+
+	var zeroes [4]byte
+	pad := (cw.wn+3)&^3 - cw.wn
+	cw.b.Write(zeroes[:pad])
+	cw.wn = 0
+}
+
+func (cw *Writer) Write(b []byte) (int, error) {
+	if cw.wn > math.MaxInt32 {
+		return 0, ErrTooLarge
+	}
+
+	if cw.wn-math.MaxInt32 < len(b) {
+		b = b[:cw.wn-math.MaxInt32]
+	}
+
+	n, err := cw.b.Write(b)
+	cw.wn += n
+	return n, err
+}
+
+func (cw *Writer) WriteHeader(hdr *Header) error {
+	cw.flush()
+
+	h := hdrnewc{
+		Magic: [6]byte{'0', '7', '0', '7', '0', '1'},
+	}
+	namesz := fmt.Sprintf("%X", len(hdr.Name)+1)
+	filesz := fmt.Sprintf("%X", hdr.Size)
+	copy(h.Namesz[:], namesz)
+	copy(h.Filesz[:], filesz)
+
+	binary.Write(cw.b, binary.LittleEndian, &h)
+	cw.b.Write([]byte(hdr.Name))
+	cw.b.Write([]byte{0x00})
+
+	var zeroes [4]byte
+	pad := (hdrnewcsz+len(hdr.Name)+1+3)&^0x3 - (hdrnewcsz + len(hdr.Name) + 1)
+	cw.b.Write(zeroes[:pad])
+
+	return nil
+}
