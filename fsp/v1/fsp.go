c9b1a5378edd3e092b5302e26c13a81bce18e880
qeed <qeed.quan@gmail.com>
2018-04-23 17:38:59 -0400

    fsp: add error codes

--- fsp/v1/fsp.go
+++ fsp/v1/fsp.go
@@ -18,3 +18,53 @@ type Header struct {
 	NotifyPhaseEntryOffset uint32
 	_                      [4]byte
 }
+
+type Error int
+
+const (
+	SUCCESS              Error = 0x00000000
+	INVALID_PARAMETER    Error = 0x80000002
+	UNSUPPORTED          Error = 0x80000003
+	NOT_READY            Error = 0x80000006
+	DEVICE_ERROR         Error = 0x80000007
+	OUT_OF_RESOURCES     Error = 0x80000009
+	VOLUME_CORRUPTED     Error = 0x8000000A
+	NOT_FOUND            Error = 0x8000000E
+	TIMEOUT              Error = 0x80000012
+	ABORTED              Error = 0x80000015
+	INCOMPATIBLE_VERSION Error = 0x80000010
+	SECURITY_VIOLATION   Error = 0x8000001A
+	CRC_ERROR            Error = 0x8000001B
+)
+
+func (e Error) Error() string {
+	switch e {
+	case SUCCESS:
+		return "success"
+	case INVALID_PARAMETER:
+		return "invalid parameter"
+	case UNSUPPORTED:
+		return "unsupported"
+	case NOT_READY:
+		return "not ready"
+	case DEVICE_ERROR:
+		return "device error"
+	case OUT_OF_RESOURCES:
+		return "out of resources"
+	case VOLUME_CORRUPTED:
+		return "volume corrupted"
+	case NOT_FOUND:
+		return "not found"
+	case TIMEOUT:
+		return "timeout"
+	case ABORTED:
+		return "aborted"
+	case INCOMPATIBLE_VERSION:
+		return "incompatible version"
+	case SECURITY_VIOLATION:
+		return "security violation"
+	case CRC_ERROR:
+		return "crc error"
+	}
+	return "unknown error"
+}
